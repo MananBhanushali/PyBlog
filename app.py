dbd75fe6e881ccf07138179cd2214f64c86589d4
Manan Bhanushali <mananbhanushali999@gmail.com>
2021-02-08 09:16:42 +0530

    Add Navbar Using Bootstrap

--- app.py
+++ app.py
@@ -5,28 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	my_name = "Manan"
-	return render_template("index.html", name=my_name)
+    my_name = "Manan"
+    return render_template("index.html", name=my_name)
 
 
 @app.route("/user/<name>")
 def user(name):
-	return render_template("user.html", user_name=name)
-    # return f"<h1>Hello {name}</h1>"	
+    return render_template("user.html", user_name=name)
 
 
 # Custom Error Pages
 
 # Page Not Found Error
 @app.errorhandler(404)
-
 def page_not_found_error(error):
-	return render_template("error404.html"), 404
+    return render_template("error404.html"), 404
+
 
 # Internal Server Error
 @app.errorhandler(500)
-
 def internal_server_error(error):
-	return render_template("error500.html"), 500
-
-
+    return render_template("error500.html"), 500
