2fdd51108fe3191fcddc329c7f6702e992ea78f1
Manan Bhanushali <mananbhanushali999@gmail.com>
2021-02-27 10:20:11 +0530

    Add Static Files in 'static' Directory

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+# Loading Environment Variables
+env_path = Path('.') / '.env'
+load_dotenv(env_path)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+# Create A Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
@@ -12,6 +29,22 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", user_name=name)
+
+
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("Form Submitted Successfully")
+
+    return render_template("name.html",
+                           name=name,
+                           form=form)
 
 
 # Custom Error Pages
